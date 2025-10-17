0bced92438258ae2420a67c992d70b84076bcddc
yuki-2525 <117993518+yuki-2525@users.noreply.github.com>
2025-10-18 00:25:08 +0900

    ヘルプボタンの実装

--- avatar-clipboard/utils/ui-manager.js
+++ avatar-clipboard/utils/ui-manager.js
@@ -378,7 +378,14 @@ class UIManager {
     windowContainer.innerHTML = `
       <div class="booth-manager-header">
         <h3>${this.getMessage('avatarClipboardTitle')}</h3>
-        <button class="booth-manager-close" type="button">×</button>
+        <div class="booth-manager-controls">
+          <a href="https://assetconnect.sakurayuki.dev/tutorial#avatar-copy" target="_blank" class="booth-help-btn" title="Help">
+            <svg class="help-icon" viewBox="0 0 24 24" width="16" height="16">
+              <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/>
+            </svg>
+          </a>
+          <button class="booth-manager-close" type="button">×</button>
+        </div>
       </div>
       <div class="booth-manager-content">
         <div class="booth-manager-notification" id="booth-notification" style="display: none;">
